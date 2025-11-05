442bd8f28b4157a92de6c6637c8bb4a36b9abfe3
KaiserFran <obregon.franco2002@gmal.com>
2025-11-05 01:14:37 -0300

    arreglo3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Container from "./components/container";
 import ProductosList from "./components/ProductosList";
 
